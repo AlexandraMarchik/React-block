b65cb3e1da951b08775689bcf04291660cfdad2c
Aleksandra <sashanext20@gmail.com>
2023-02-15 21:19:58 +0300

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,18 @@ import { ButtonType } from "./components/Button/Button";
 import User from "./components/User";
 import Title from "./components/Title";
 
-
-
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button
-          title={"Primary"}
-          type={ButtonType.Primary}
-          onClick={() => {}}
-        />
-        <Button
-          title={"Secondary"}
-          type={ButtonType.Secondary}
-          onClick={() => {}}
-        />
-        <Button title={"Error"} type={ButtonType.Error} onClick={() => {}} />
-        <User userName={"Artem Malkin"}/>
-        <Title title={"Blog"}/>
-      </header>
+    <div>
+      <Button title={"Primary"} type={ButtonType.Primary} onClick={() => {}} />
+      <Button
+        title={"Secondary"}
+        type={ButtonType.Secondary}
+        onClick={() => {}}
+      />
+      <Button title={"Error"} type={ButtonType.Error} onClick={() => {}} />
+      <User userName={"Artem Malkin"} />
+      <Title title={"Blog"} />
     </div>
   );
 };
